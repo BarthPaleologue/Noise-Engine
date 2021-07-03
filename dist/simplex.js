a4081d87ecac02478d11781c1b6b687c865a859c
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2021-07-03 15:25:32 +0200

    Basically it

---
+++ dist/simplex.js
@@ -0,0 +1,41 @@
+export class Simplex extends BABYLON.PostProcess {
+    constructor(name, camera, scene) {
+        super(name, "./simplex", [
+            "time",
+            "nbOctaves",
+            "decay",
+            "lacunarity",
+            "power",
+            "nbDomainWarping",
+            "domainWarpingStrength",
+            "frequency",
+            "showLevelLines",
+            "nbLines"
+        ], [], 1, camera, BABYLON.Texture.BILINEAR_SAMPLINGMODE, scene.getEngine());
+        this.settings = {
+            nbOctaves: 5,
+            decay: 2.0,
+            lacunarity: 2.0,
+            power: 1.0,
+            nbDomainWarping: 0,
+            domainWarpingStrength: 5.0,
+            frequency: 10,
+            showLevelLines: false,
+            nbLines: 5,
+        };
+        let time = 0.0;
+        this.onBeforeRender = (effect) => {
+            time += 0.002;
+            effect.setFloat("time", time);
+            effect.setInt("nbOctaves", this.settings.nbOctaves);
+            effect.setFloat("decay", this.settings.decay);
+            effect.setFloat("lacunarity", this.settings.lacunarity);
+            effect.setFloat("power", this.settings.power);
+            effect.setInt("nbDomainWarping", this.settings.nbDomainWarping);
+            effect.setFloat("domainWarpingStrength", this.settings.domainWarpingStrength);
+            effect.setFloat("frequency", this.settings.frequency);
+            effect.setBool("showLevelLines", this.settings.showLevelLines);
+            effect.setInt("nbLines", this.settings.nbLines);
+        };
+    }
+}
---
+++ src/simplex.ts
@@ -0,0 +1,58 @@
+
+interface simplexSettings {
+    nbOctaves: number;
+    decay: number;
+    lacunarity: number;
+    power: number;
+    nbDomainWarping: number;
+    domainWarpingStrength: number;
+    frequency: number;
+    showLevelLines: boolean;
+    nbLines: number;
+}
+
+export class Simplex extends BABYLON.PostProcess {
+    settings: simplexSettings;
+    constructor(name: string, camera: BABYLON.Camera, scene: BABYLON.Scene) {
+        super(name, "./simplex", [
+            "time",
+            "nbOctaves",
+            "decay",
+            "lacunarity",
+            "power",
+            "nbDomainWarping",
+            "domainWarpingStrength",
+            "frequency",
+            "showLevelLines",
+            "nbLines"
+        ], [], 1, camera, BABYLON.Texture.BILINEAR_SAMPLINGMODE, scene.getEngine());
+
+        this.settings = {
+            nbOctaves: 5,
+            decay: 2.0,
+            lacunarity: 2.0,
+            power: 1.0,
+            nbDomainWarping: 0,
+            domainWarpingStrength: 5.0,
+            frequency: 10,
+            showLevelLines: false,
+            nbLines: 5,
+        };
+
+        let time = 0.0;
+
+        this.onBeforeRender = (effect: BABYLON.Effect) => {
+            time += 0.002;
+            effect.setFloat("time", time);
+            effect.setInt("nbOctaves", this.settings.nbOctaves);
+            effect.setFloat("decay", this.settings.decay);
+            effect.setFloat("lacunarity", this.settings.lacunarity);
+            effect.setFloat("power", this.settings.power);
+            effect.setInt("nbDomainWarping", this.settings.nbDomainWarping);
+            effect.setFloat("domainWarpingStrength", this.settings.domainWarpingStrength);
+            effect.setFloat("frequency", this.settings.frequency);
+            effect.setBool("showLevelLines", this.settings.showLevelLines);
+            effect.setInt("nbLines", this.settings.nbLines);
+        };
+    }
+}
